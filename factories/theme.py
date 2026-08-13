e517bc79dc921cec69491c2edcb66d3a8a718d9d
Fernito69 <fernito@gmail.com>
2026-08-13 17:25:39 +0200

    Some more color refactoring

--- factories/theme.py
+++ factories/theme.py
@@ -5,46 +5,53 @@ COLORS
 """
 
 
+# Receives a float between 0 and 1
 def _normalize(v: float) -> int:
-    return int(max(min(v, 1), v, 0) * 255)
+    return round(max(min(v, 1), v, 0))
 
 
-_DEFAULT_INTENSITY = 0.7
+# intensity can go from 0% to 100% -> 0 to 1
+_DEFAULT_INTENSITY: float = 0.7
+
+
+def MakeColor(
+    intensity: float = _DEFAULT_INTENSITY,
+    rgb_values: tuple[int, int, int] = (255, 255, 255),
+):
+
+    r = _normalize(intensity * rgb_values[0])
+    g = _normalize(intensity * rgb_values[1])
+    b = _normalize(intensity * rgb_values[2])
+
+    return RGB(r, g, b)
 
 
 def Green(intensity: float = _DEFAULT_INTENSITY) -> RGB:
-    i = _normalize(intensity)
-    return RGB(0, i, 0)
+    return MakeColor(intensity, (0, 255, 0))
 
 
 def Red(intensity: float = _DEFAULT_INTENSITY) -> RGB:
-    i = _normalize(intensity)
-    return RGB(i, 0, 0)
+    return MakeColor(intensity, (255, 0, 0))
 
 
 def Yellow(intensity: float = _DEFAULT_INTENSITY) -> RGB:
-    i = _normalize(intensity)
-    return RGB(i, i, 0)
+    return MakeColor(intensity, (255, 255, 0))
 
 
 def Blue(intensity: float = _DEFAULT_INTENSITY) -> RGB:
-    i = _normalize(intensity)
-    return RGB(0, 0, i)
+    return MakeColor(intensity, (0, 0, 255))
 
 
 def White(intensity: float = _DEFAULT_INTENSITY) -> RGB:
-    i = _normalize(intensity)
-    return RGB(i, i, i)
+    return MakeColor(intensity, (255, 255, 255))
 
 
 def Cyan(intensity: float = _DEFAULT_INTENSITY) -> RGB:
-    i = _normalize(intensity)
-    return RGB(0, i, i)
+    return MakeColor(intensity, (0, 255, 255))
 
 
 def Magenta(intensity: float = _DEFAULT_INTENSITY) -> RGB:
-    i = _normalize(intensity)
-    return RGB(i, 0, i)
+    return MakeColor(intensity, (255, 0, 255))
 
 
 """
