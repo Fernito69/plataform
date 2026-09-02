495e7bec368c963dc17bb12bf46437771ba0f39c
Fernando Calvo <fernando.calvo@agrinorm.ai>
2026-09-02 08:20:28 +0200

    Add VectorF class

--- model/base.py
+++ model/base.py
@@ -34,3 +34,45 @@ class DistVector3D(DistCoordBase):
 class Orientation(StrEnum):
     HORIZONTAL = auto()
     VERTICAL = auto()
+
+
+# TODO: refactor using these vectors:
+@dataclass
+class VectorF:
+    x: float
+    y: float
+    z: float | None = None
+
+    def __neg__(self) -> "VectorF":
+        return VectorF(x=-self.x, y=-self.y, z=-self.z if self.z else None)
+
+    def __add__(self, other: "VectorF") -> "VectorF":
+        return VectorF(
+            x=self.x + other.x,
+            y=self.y + other.y,
+            z=(self.z + other.z if self.z is not None and other.z is not None else None),
+        )
+
+    def __sub__(self, other: "VectorF") -> "VectorF":
+        return VectorF(
+            x=self.x - other.x,
+            y=self.y - other.y,
+            z=(self.z - other.z if self.z is not None and other.z is not None else None),
+        )
+
+    def __mul__(self, scalar: float) -> "VectorF":
+        return VectorF(
+            self.x * scalar,
+            self.y * scalar,
+            None if self.z is None else self.z * scalar,
+        )
+
+    def __rmul__(self, scalar: float) -> "VectorF":
+        return self * scalar
+
+    def __abs__(self) -> float:
+        """get magnitude"""
+        return (self.x**2 + self.y**2 + (self.z**2 if self.z else 0)) ** 0.5
+
+
+class PointF(VectorF): ...
