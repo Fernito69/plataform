25a624a606ffa765c6b841e0008f9eb0ad796c4a
Fernito69 <fernito@gmail.com>
2026-09-18 12:51:08 +0200

    Add missing files

---
+++ physics2d/shape/base.py
@@ -0,0 +1,146 @@
+from abc import abstractmethod
+from typing import TYPE_CHECKING
+
+from model.base import PointF, VectorF
+from model.theme import RGB, Theme
+from physics2d.constants import DEFAULT_GRAVITY_ACCELERATION
+from physics2d.model.shared import RenderInfo
+from utils import random_offset
+
+if TYPE_CHECKING:
+    from physics2d.physics2d import Physics2D
+
+
+class Shape:
+    name: str
+    theme: Theme
+    secondary_theme: Theme | None
+
+    # in radians
+    angle: float
+    center_of_mass: PointF
+    _affected_by_gravity: bool
+    _own_gravity_accel: float | None
+    velocity: VectorF
+    _last_known_direction: VectorF
+    angular_velocity: float
+    # if > 0, it floats around randomly, like brownian motion
+    floating_multi: float
+
+    density: float
+    volume: float
+    weight: float
+
+    is_collideable: bool
+
+    affected_by_friction: bool
+
+    """renders behind the player even if in the foreground layer"""
+    render_behind_player: bool
+
+    def __init__(
+        self,
+        center_of_mass: PointF,
+        name: str,
+        density: float,
+        volume: float,
+        theme: Theme = Theme(),
+        angle: float = 0,
+        affected_by_gravity: bool = False,
+        initial_velocity: VectorF = VectorF(0, 0),
+        initial_angular_velocity: float = 0,
+        own_gravity: float | None = None,
+        secondary_theme: Theme | None = None,
+        floating_multi: float = 0,
+        is_collideable: bool = False,
+        affected_by_friction: bool = False,
+        render_behind_player: bool = False,
+    ):
+        self.theme = theme
+        self.secondary_theme = secondary_theme
+        self.name = name
+        self.angle = angle
+        self._affected_by_gravity = affected_by_gravity
+        self.velocity = initial_velocity
+        self._own_gravity_accel = own_gravity
+        self.floating_multi = floating_multi
+        self.angular_velocity = initial_angular_velocity
+        self.center_of_mass = center_of_mass
+        self.density = density
+        self.volume = volume
+        self.weight = density * volume
+        self.is_collideable = is_collideable
+        self.affected_by_friction = affected_by_friction
+        self.render_behind_player = render_behind_player
+
+    def _apply_gravity(self, gravity_accel: float = DEFAULT_GRAVITY_ACCELERATION) -> None:
+        if not self._affected_by_gravity and not self._own_gravity_accel:
+            return
+        self.velocity = VectorF(
+            self.velocity.x,
+            self.velocity.y - (self._own_gravity_accel or gravity_accel),
+        )
+
+    def set_last_known_direction(self) -> None:
+        # HACK, let's see if helps
+        if abs(self.velocity.x) < 0.1 and abs(self.velocity.y) < 0.1:
+            return
+        self._last_known_direction = self.velocity.unit_vector()
+
+    def get_last_known_direction(self, scale: float = 1) -> VectorF:
+        raw = self._last_known_direction or self.velocity.unit_vector()
+        return (scale * raw).as_vector() if scale != 1 else raw
+
+    @abstractmethod
+    def get_render_info(cls) -> list[RenderInfo]:
+        # Each shape should do its thing
+        raise NotImplementedError(f"Shape must have a get_render_info method")
+
+    @abstractmethod
+    def would_collide_with(self, shape: "Shape", engine: "Physics2D") -> bool:
+        """Determines wheter the current shape would collide with a particular shape, given their location"""
+
+        # Each shape should do its thing
+        raise NotImplementedError(f"Shape must have a would_collide method")
+
+    @abstractmethod
+    def apply_angular_momentum(self, momentum: VectorF) -> None:
+        # TODO: implement
+        pass
+
+    @abstractmethod
+    def update_center_of_mass(cls) -> None:
+        # Each entity should do its thing
+        raise NotImplementedError(
+            f"{cls.name or 'UnknownPiece'} must have a calc_center_of_mass method"
+        )
+
+    @abstractmethod
+    def rotate(cls) -> None:
+        # Each entity should do its thing
+        raise NotImplementedError(f"{cls.name or 'UnknownPiece'} must have a rotate method")
+
+    @abstractmethod
+    def _get_color(cls, x: int | None = None, y: int | None = None) -> RGB:
+        # Each entity should do its thing
+        raise NotImplementedError(f"{cls.name or 'UnknownPiece'} must have a _get_color method")
+
+    def _float_around(self) -> None:
+        if self.floating_multi == 0:
+            return
+
+        self.velocity = (
+            self.velocity
+            + VectorF(self.floating_multi * random_offset(), self.floating_multi * random_offset())
+        ).as_vector()
+
+    @abstractmethod
+    def _apply_movement(cls, scenario: "Physics2D") -> None:
+        # Each entity should do its thing
+        raise NotImplementedError(
+            f"{cls.name or 'UnknownPiece'} must have an apply_movement method"
+        )
+
+    def do_your_thing(self, engine: "Physics2D") -> None:
+        self._apply_gravity(engine.scenario.gravity_acceleration)
+        self._apply_movement(engine)
---
+++ physics2d/shape/rectangle.py
@@ -0,0 +1,173 @@
+import math
+from typing import TYPE_CHECKING
+
+from factories.theme import RGB, White
+from model.base import PointF, VectorF
+from model.theme import Theme
+from physics2d.model.shared import RenderInfo
+from physics2d.shape.line import Line
+
+if TYPE_CHECKING:
+    from physics2d.physics2d import Physics2D
+
+
+# TODO: should have its own rectangle Shape
+class Rectangle(Line):
+    vertices: tuple[PointF, PointF]
+
+    def __init__(
+        self,
+        vertices: tuple[PointF, PointF],
+        theme: Theme = Theme(),
+        angle: float = 0,
+        affected_by_gravity: bool = False,
+        initial_velocity: VectorF = VectorF(0, 0),
+        own_gravity: float | None = None,
+        secondary_theme: Theme | None = None,
+        floating_multi: float = 0,
+        initial_angular_velocity: float = 0,
+        density: float = 1,
+    ):
+        super().__init__(
+            theme=theme,
+            angle=angle,
+            affected_by_gravity=affected_by_gravity,
+            initial_velocity=initial_velocity,
+            own_gravity=own_gravity,
+            secondary_theme=secondary_theme,
+            floating_multi=floating_multi,
+            initial_angular_velocity=initial_angular_velocity,
+            points=vertices,
+            density=density,
+        )
+        self.theme = theme
+        self.secondary_theme = secondary_theme
+        self.vertices = vertices
+        self.initial_angular_velocity = initial_angular_velocity
+        self._own_gravity_accel = own_gravity
+        self.floating_multi = floating_multi
+        self.velocity = initial_velocity
+        self.angular_velocity = initial_angular_velocity
+        self._affected_by_gravity = affected_by_gravity
+
+        min_x, max_x, min_y, max_y = self._get_sorted_vertices()
+        self.volume = (max_x - min_x) * (max_y - min_y)
+        self.density = density
+
+    def _get_sorted_vertices(self) -> tuple[float, float, float, float]:
+        min_x, max_x = sorted(
+            (
+                self.vertices[0].x,
+                self.vertices[1].x,
+            )
+        )
+        min_y, max_y = sorted(
+            (
+                self.vertices[0].y,
+                self.vertices[1].y,
+            )
+        )
+        return (min_x, max_x, min_y, max_y)
+
+    def _apply_movement(self, engine: "Physics2D") -> None:
+        self._float_around()
+
+        if not any(a != 0 for a in self.velocity):
+            return
+
+        self.vertices = (
+            (self.vertices[0] + self.velocity),
+            (self.vertices[1] + self.velocity),
+        )
+        self.center_of_mass = self.center_of_mass + self.velocity
+
+    def rotate(self) -> None:
+        pass
+
+    def do_your_thing(self, engine: "Physics2D") -> None:
+        self._apply_gravity(engine.scenario.gravity_acceleration)
+        self._apply_movement(engine)
+
+    def _get_color(self, x: int | None = None, y: int | None = None) -> RGB:
+        if not self.secondary_theme:
+            return self.theme.color or White()
+
+        # check which direction is the widest
+
+        vertex_1, vertex_2 = self.vertices
+        width = abs(vertex_1.x - vertex_2.x)
+        height = abs(vertex_1.y - vertex_2.y)
+
+        apply_gradient_horizontally: bool = width >= height
+        if (
+            apply_gradient_horizontally
+            and x is None
+            or not apply_gradient_horizontally
+            and y is None
+        ):
+            raise IndexError("What's wrong with you?")
+
+        color_ratio: float = (
+            abs(vertex_1.x - x) / width
+            if apply_gradient_horizontally and x is not None
+            else abs(vertex_1.y - y) / height
+            if y is not None
+            else 0
+        )
+
+        color: RGB = (
+            (self.theme.color or White())
+            .with_intensity(color_ratio)
+            .mix_with((self.secondary_theme.color or White()).with_intensity(1 - color_ratio))
+        )
+
+        # mix_colors(
+        #     [
+        #         (self.theme.color or White()).with_intensity(color_ratio),
+        #         (self.secondary_theme.color or White()).with_intensity(1 - color_ratio),
+        #     ]
+        # )
+
+        return color
+
+    def get_render_info(self) -> list[RenderInfo]:
+        piece_info = []
+        min_x, max_x, min_y, max_y = self._get_sorted_vertices()
+
+        _DELTA = 1
+
+        for x in range(math.floor(min_x), math.ceil(max_x)):
+            for y in range(math.floor(min_y), math.ceil(max_y)):
+                # TODO: make this with angles, it gets a bit more tricky
+                if min_x > x > max_x or min_y > y > max_y:
+                    continue
+
+                distance_left_x = abs(x - min_x + 1)
+                distance_right_x = abs(x - max_x)
+                distance_down_y = abs(y - min_y + 1)
+                distance_up_y = abs(y - max_y)
+
+                distance_x = (
+                    1 - distance_left_x
+                    if distance_left_x < _DELTA
+                    else 1 - distance_right_x
+                    if distance_right_x < _DELTA
+                    else 0
+                )
+                distance_y = (
+                    1 - distance_up_y
+                    if distance_up_y < _DELTA
+                    else 1 - distance_down_y
+                    if distance_down_y < _DELTA
+                    else 0
+                )
+
+                piece_info.append(
+                    RenderInfo(
+                        distance_to_pixel_center=abs(VectorF(distance_x, distance_y)),
+                        color=self._get_color(x, y).with_intensity(),
+                        point=PointF(x, y),
+                    )
+                )
+
+        return piece_info
