7e3e9d816431d269cb25d78e79ee8a9ea24be531
Feño <fernito@gmail.com>
2026-09-20 13:09:34 +0200

    Fix crosshair center

--- physics2d/entities/crosshair.py
+++ physics2d/entities/crosshair.py
@@ -68,7 +68,7 @@ class Crosshair(PhysicsEntity, MouseHandler):
             _make_line(((-_HAIR_OFFSET, 0), (-_HAIR_OFFSET - _HAIR_SIZE, 0))),
         ]
         # give a lil offset:
-        self.center -= PointF(_DOT_SIZE, _DOT_SIZE)
+        self.center -= PointF(_DOT_SIZE/2, _DOT_SIZE/2)
 
     ##############
     """MOVEMENT"""
